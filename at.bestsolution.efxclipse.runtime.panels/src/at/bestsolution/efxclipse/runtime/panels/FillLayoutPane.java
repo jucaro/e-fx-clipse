05f343ab293052d25972c706aecf9986d81eabff
Tom Schindl <tom.schindl@bestsolution.at>
2011-09-22 23:43:06 +0200

    added sash layout

--- at.bestsolution.efxclipse.runtime.panels/src/at/bestsolution/efxclipse/runtime/panels/FillLayoutPane.java
+++ at.bestsolution.efxclipse.runtime.panels/src/at/bestsolution/efxclipse/runtime/panels/FillLayoutPane.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Tom Schindl<tom.schindl@bestsolution.at> - ported to JavaFX
+ *******************************************************************************/
 package at.bestsolution.efxclipse.runtime.panels;
 
 import javafx.beans.property.SimpleBooleanProperty;
