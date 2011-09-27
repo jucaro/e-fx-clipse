3e44db48e59ec7c4d30cf731d24257116fe760e9
Tom Schindl <tom.schindl@bestsolution.at>
2011-09-28 01:54:43 +0200

    fixed package imports

--- at.bestsolution.efxclipse.tooling.pde.ui/src/at/bestsolution/efxclipse/tooling/pde/ui/FXOSGiAppTemplate.java
+++ at.bestsolution.efxclipse.tooling.pde.ui/src/at/bestsolution/efxclipse/tooling/pde/ui/FXOSGiAppTemplate.java
@@ -17,11 +17,15 @@ import org.eclipse.pde.core.plugin.IPluginElement;
 import org.eclipse.pde.core.plugin.IPluginExtension;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
 import org.eclipse.pde.core.plugin.IPluginReference;
+import org.eclipse.pde.internal.core.bundle.BundlePluginBase;
+import org.eclipse.pde.internal.core.ibundle.IBundle;
 import org.eclipse.pde.ui.IFieldData;
 import org.eclipse.pde.ui.templates.PluginReference;
+import org.osgi.framework.Constants;
 
 import at.bestsolution.efxclipse.tooling.jdt.core.JavaFXCore;
 
+@SuppressWarnings("restriction")
 public class FXOSGiAppTemplate extends FXPDETemplateSection {
 	public static final String KEY_APPLICATION_CLASS = "applicationClass"; //$NON-NLS-1$
 	public static final String KEY_WINDOW_TITLE = "windowTitle"; //$NON-NLS-1$
@@ -127,11 +131,25 @@ public class FXOSGiAppTemplate extends FXPDETemplateSection {
 		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>(Arrays.asList(jProject.getRawClasspath()));
 		entries.add(JavaCore.newContainerEntry(JavaFXCore.JAVAFX_CONTAINER_PATH));
 		jProject.setRawClasspath(entries.toArray(new IClasspathEntry[0]), monitor);
+		
+		if( model.getPluginBase() instanceof BundlePluginBase ) {
+			IBundle bundle = ((BundlePluginBase) model.getPluginBase()).getBundle();
+			
+			String imports = bundle.getHeader(Constants.IMPORT_PACKAGE);
+			if( imports == null ) {
+				imports = "";
+			}
+			
+			imports += getCommaValuesFromPackagesArray(getImports(), null);
+			
+			bundle.setHeader(Constants.IMPORT_PACKAGE, imports);
+		}
+		
 		super.execute(project, model, monitor);
 	}
 	
 	public IPluginReference[] getDependencies(String schemaVersion) {
-		IPluginReference[] dep = new IPluginReference[2];
+		IPluginReference[] dep = new IPluginReference[3];
 		dep[0] = new PluginReference("org.eclipse.core.runtime", null, 0); //$NON-NLS-1$
 		dep[1] = new PluginReference("at.bestsolution.efxclipse.runtime.application", null, 0); //$NON-NLS-1$
 		
@@ -150,5 +168,53 @@ public class FXOSGiAppTemplate extends FXPDETemplateSection {
 		if (copyBrandingDirectory())
 			return new String[] {"icons/", "splash.bmp"}; //$NON-NLS-1$ //$NON-NLS-2$
 		return super.getNewFiles();
+	}
+	
+	protected String getCommaValuesFromPackagesArray(String[] values, String version) {
+		StringBuffer buffer = new StringBuffer();
+		for (String value : values) {
+			if (buffer.length() > 0) {
+				buffer.append(",\n "); //$NON-NLS-1$ // space required for multiline headers
+			}
+			buffer.append(value);
+
+			if (value.indexOf(";version=") == -1 && (version != null) && (values.length == 1)) { //$NON-NLS-1$
+				buffer.append(";version=\"").append(version).append("\""); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+		}
+		return buffer.toString();
+	}
+	
+	private static String[] getImports() {
+		return new String[] {
+				"javafx.animation;version=\"2.0.0\"",
+				"javafx.application;version=\"2.0.0\"",
+				"javafx.beans;version=\"2.0.0\"",
+				"javafx.beans.binding;version=\"2.0.0\"",
+				"javafx.beans.property;version=\"2.0.0\"",
+				"javafx.beans.value;version=\"2.0.0\"",
+				"javafx.collections;version=\"2.0.0\"",
+				"javafx.concurrent;version=\"2.0.0\"",
+				"javafx.embed.swing;version=\"2.0.0\"",
+				"javafx.event;version=\"2.0.0\"",
+				"javafx.fxml;version=\"2.0.0\"",
+				"javafx.geometry;version=\"2.0.0\"",
+				"javafx.scene;version=\"2.0.0\"",
+				"javafx.scene.chart;version=\"2.0.0\"",
+				"javafx.scene.control;version=\"2.0.0\"",
+				"javafx.scene.control.cell;version=\"2.0.0\"",
+				"javafx.scene.effect;version=\"2.0.0\"",
+				"javafx.scene.image;version=\"2.0.0\"",
+				"javafx.scene.input;version=\"2.0.0\"",
+				"javafx.scene.layout;version=\"2.0.0\"",
+				"javafx.scene.media;version=\"2.0.0\"",
+				"javafx.scene.paint;version=\"2.0.0\"",
+				"javafx.scene.shape;version=\"2.0.0\"",
+				"javafx.scene.text;version=\"2.0.0\"",
+				"javafx.scene.transform;version=\"2.0.0\"",
+				"javafx.scene.web;version=\"2.0.0\"",
+				"javafx.stage;version=\"2.0.0\"",
+				"javafx.util;version=\"2.0.0\""
+			};
 	}
 }
