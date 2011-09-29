18e6144777e2684ddecc71eef88881325f746e1c
Tom Schindl <tom.schindl@bestsolution.at>
2011-09-29 17:15:29 +0200

    removed custom code and only use the WinRegistry class

--- at.bestsolution.efxclipse.runtime.osgi/src/at/bestsolution/efxclipse/runtime/osgi/FXClassLoader.java
+++ at.bestsolution.efxclipse.runtime.osgi/src/at/bestsolution/efxclipse/runtime/osgi/FXClassLoader.java
@@ -1,9 +1,7 @@
 package at.bestsolution.efxclipse.runtime.osgi;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.ProtectionDomain;
@@ -11,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.prefs.Preferences;
 
 import org.eclipse.osgi.baseadaptor.BaseData;
 import org.eclipse.osgi.baseadaptor.bundlefile.BundleEntry;
@@ -74,44 +71,6 @@ public class FXClassLoader implements ClassLoadingHook {
 	static class MyBundleClassLoader extends DefaultClassLoader {
 		private URLClassLoader fxClassLoader;
 		
-		private static String readKey(String key, String value) throws Exception {
-			int HKEY_LOCAL_MACHINE = 0x80000002; // WindowsPreferences#HKEY_LOCAL_MACHINE
-			int KEY_READ = 0x20019; // WindowsPreferences#KEY_READ
-			int REG_SUCCESS = 0; // WindowsPreferences#REG_SUCCESS
-
-			Preferences root = Preferences.systemRoot();
-
-//			int[] handles = root.WindowsRegOpenKey(HKEY_LOCAL_MACHINE, toNullTerminatedString(key),KEY_READ);
-//			byte[] byteValue = root.WindowsRegQueryValueEx(handles[0],toNullTerminatedString(value));
-//			root.WindowsRegCloseKey(handles[0]);
-			
-			Method WindowsRegOpenKey = Preferences.systemRoot().getClass().getDeclaredMethod("WindowsRegOpenKey", int.class, byte[].class, int.class );
-			WindowsRegOpenKey.setAccessible(true);
-			
-			Method WindowsRegQueryValueEx = Preferences.systemRoot().getClass().getDeclaredMethod("WindowsRegQueryValueEx", int.class, byte[].class );
-			WindowsRegQueryValueEx.setAccessible(true);
-			
-			Method WindowsRegCloseKey = Preferences.systemRoot().getClass().getDeclaredMethod("WindowsRegCloseKey", int.class );
-			WindowsRegCloseKey.setAccessible(true);
-			
-			int[] handles = (int[]) WindowsRegOpenKey.invoke(root, new Object[] { Integer.valueOf(HKEY_LOCAL_MACHINE), toNullTerminatedString(key), Integer.valueOf(KEY_READ) });
-			if (handles[1] != REG_SUCCESS) {
-				return null;
-			}
-			byte[] byteValue = (byte[]) WindowsRegQueryValueEx.invoke(root, new Object[] { Integer.valueOf(handles[0]), toNullTerminatedString(value) });
-			WindowsRegCloseKey.invoke(root, new Object[] { Integer.valueOf(handles[0]) });
-			return (byteValue != null ? new String(byteValue).trim() : null);
-		}
-		
-		private static byte[] toNullTerminatedString(String str) throws IOException {
-			ByteArrayOutputStream stream = new ByteArrayOutputStream(str.length()+1);
-			stream.write(str.getBytes());
-			stream.write(0);
-			byte[] rv = stream.toByteArray();
-			stream.close();
-			return rv;
-		}
-		
 		public MyBundleClassLoader(ClassLoader parent,
 				ClassLoaderDelegate delegate, ProtectionDomain domain,
 				BaseData bundledata, String[] classpath) throws Exception {
@@ -148,7 +107,7 @@ public class FXClassLoader implements ClassLoadingHook {
 			}
 			
 			String versionString = effectiveVersion.getMajor() + "."+effectiveVersion.getMinor()+"." + effectiveVersion.getMicro();
-			String installPath = readKey("Software\\Oracle\\JavaFX\\" + versionString, "Path");
+			String installPath = WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE,"Software\\Oracle\\JavaFX\\" + versionString, "Path");
 			if( installPath != null ) {
 				File f = new File(installPath + "/lib/jfxrt.jar");
 				
