dce6f7dd825bd34619d95be6d73c1c9d714bcd75
Tom Schindl <tom.schindl@bestsolution.at>
2011-10-04 14:44:49 +0200

    adjusted template to new API

--- at.bestsolution.efxclipse.tooling.pde.ui/templates_3.6/javaFXApp/java/$applicationClass$.java
+++ at.bestsolution.efxclipse.tooling.pde.ui/templates_3.6/javaFXApp/java/$applicationClass$.java
@@ -1,5 +1,7 @@
 package $packageName$;
 
+import org.eclipse.equinox.app.IApplicationContext;
+
 import javafx.stage.Stage;
 import at.bestsolution.efxclipse.runtime.application.AbstractJFXApplication;
 
@@ -7,7 +9,7 @@ import at.bestsolution.efxclipse.runtime.application.AbstractJFXApplication;
  * This class controls all aspects of the JavaFX OSGi application's execution
  */
 public class $applicationClass$ extends AbstractJFXApplication {
-	protected void jfxStart(Stage primaryStage) {
+	protected void jfxStart(IApplicationContext applicationContext, javafx.application.Application jfxApplication, Stage primaryStage) {
 		primaryStage.setTitle("$windowTitle$");
 	}
 }
