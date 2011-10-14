d839a61ae8ac138b1925ddaeea0d0f9e1d31642f
Tom Schindl <tom.schindl@bestsolution.at>
2011-10-14 20:53:03 +0200

    new bundle which holds utilities who are used inside OSGi

---
+++ at.bestsolution.efxclipse.runtime.osgi.util/src/at/bestsolution/efxclipse/runtime/osgi/util/OSGiFXMLLoader.java
@@ -0,0 +1,81 @@
+package at.bestsolution.efxclipse.runtime.osgi.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.wiring.BundleWiring;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.util.BuilderFactory;
+
+public class OSGiFXMLLoader {
+	public static <O> O load(Class<?> requester, String relativeFxmlPath, ResourceBundle resourceBundle, BuilderFactory builderFactory) throws IOException {
+		URL url = requester.getResource(relativeFxmlPath);
+		ClassLoader loader = requester.getClassLoader();
+		return load(loader, url, resourceBundle, builderFactory);
+	}
+	
+	public static <O> O load(Bundle bundle, String bundleRelativeFxmlPath, ResourceBundle resourceBundle, BuilderFactory builderFactory) throws IOException {
+		URL url = bundle.getResource(bundleRelativeFxmlPath);
+		ClassLoader loader = bundle.adapt(BundleWiring.class).getClassLoader();
+		return load(loader, url, resourceBundle, builderFactory);
+	}
+	
+	public static <O> O load(ClassLoader classloader, URL url, ResourceBundle resourceBundle, BuilderFactory builderFactory) throws IOException {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		
+		try {
+			Thread.currentThread().setContextClassLoader(classloader);
+			O o = FXMLLoader.load(url, resourceBundle, builderFactory);
+			return o;
+		} finally {
+			Thread.currentThread().setContextClassLoader(cl);
+		}
+	}
+	
+	public static <O> O load(Class<?> requester, String relativeFxmlPath, ResourceBundle resourceBundle, BuilderFactory builderFactory, FXMLLoaderProcessor postprocessor) throws IOException {
+		URL url = requester.getResource(relativeFxmlPath);
+		ClassLoader loader = requester.getClassLoader();
+		return load(loader, url, resourceBundle, builderFactory, postprocessor);
+	}
+	
+	public static <O> O load(Bundle bundle, String bundleRelativeFxmlPath, ResourceBundle resourceBundle, BuilderFactory builderFactory, FXMLLoaderProcessor postprocessor) throws IOException {
+		URL url = bundle.getResource(bundleRelativeFxmlPath);
+		ClassLoader loader = bundle.adapt(BundleWiring.class).getClassLoader();
+		return load(loader, url, resourceBundle, builderFactory, postprocessor);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <O> O load(ClassLoader classloader, URL url, ResourceBundle resourceBundle, BuilderFactory builderFactory, FXMLLoaderProcessor postprocessor) throws IOException {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		InputStream in = null;
+		
+		try {
+			Thread.currentThread().setContextClassLoader(classloader);
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(url);
+			loader.setBuilderFactory(new JavaFXBuilderFactory());
+			in = url.openStream();
+			O value = (O) loader.load(in);
+			in.close();
+			
+			postprocessor.postProcess(loader);
+			
+			return value;
+		} finally {
+			Thread.currentThread().setContextClassLoader(cl);
+			if( in != null ) {
+				in.close();
+			}
+		}
+	}
+	
+	public interface FXMLLoaderProcessor {
+		public void postProcess(FXMLLoader loader);
+	}
+}
