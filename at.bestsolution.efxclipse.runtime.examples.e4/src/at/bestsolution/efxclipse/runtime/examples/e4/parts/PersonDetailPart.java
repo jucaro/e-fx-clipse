788d5d5e5fe9c9f4dcdc14e076fe9c6fb947511d
Tom Schindl <tom.schindl@bestsolution.at>
2011-10-17 10:03:33 +0200

    * added databinding * added effects

--- at.bestsolution.efxclipse.runtime.examples.e4/src/at/bestsolution/efxclipse/runtime/examples/e4/parts/PersonDetailPart.java
+++ at.bestsolution.efxclipse.runtime.examples.e4/src/at/bestsolution/efxclipse/runtime/examples/e4/parts/PersonDetailPart.java
@@ -6,25 +6,13 @@ import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 import at.bestsolution.efxclipse.runtime.di.FXMLBuilder;
 import at.bestsolution.efxclipse.runtime.di.FXMLLoader;
 import at.bestsolution.efxclipse.runtime.di.FXMLLoaderFactory;
 
 public class PersonDetailPart {
-	
-	@Inject
-	public PersonDetailPart(BorderPane parent) {
-//		try {
-//			Node n = FXMLLoader.load(PersonDetailPart.class.getResource("personform.fxml"), null, new JavaFXBuilderFactory());
-//			parent.setCenter(n);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	}
-	
+
 	@PostConstruct
 	void init(BorderPane parent, @FXMLLoader FXMLLoaderFactory factory) {
 		try {
---
+++ at.bestsolution.efxclipse.runtime.examples.e4/src/at/bestsolution/efxclipse/runtime/examples/e4/parts/controllers/PersonDetailPartController.java
@@ -0,0 +1,88 @@
+package at.bestsolution.efxclipse.runtime.examples.e4.parts.controllers;
+
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeanProperties;
+import org.eclipse.core.databinding.beans.IBeanValueProperty;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.e4.core.di.annotations.Optional;
+
+import at.bestsolution.efxclipse.runtime.databinding.IJFXBeanValueProperty;
+import at.bestsolution.efxclipse.runtime.databinding.JFXBeanProperties;
+import at.bestsolution.efxclipse.runtime.di.FXMLLoaderFactory;
+import at.bestsolution.efxclipse.runtime.examples.e4.model.Person;
+
+@SuppressWarnings("restriction")
+public class PersonDetailPartController {
+	private IObservableValue master = new WritableValue();
+	
+	@FXML
+	TextField firstname;
+	
+	@FXML
+	TextField lastname;
+	
+	@FXML
+	TextField street;
+	
+	@FXML
+	TextField zip;
+	
+	@FXML
+	TextField city;
+
+	private FadeTransition fadeOutTransition;
+
+	private FadeTransition fadeInTransition;
+	
+	@PostConstruct
+	void init(@Named(FXMLLoaderFactory.CONTEXT_KEY) GridPane rootPane) {
+		IJFXBeanValueProperty uiProp = JFXBeanProperties.value("text");
+		
+		DataBindingContext ctx = new DataBindingContext();
+		ctx.bindValue(uiProp.observe(firstname), BeanProperties.value("firstname").observeDetail(master));
+		ctx.bindValue(uiProp.observe(lastname), BeanProperties.value("lastname").observeDetail(master));
+		ctx.bindValue(uiProp.observe(street), BeanProperties.value("street").observeDetail(master));
+		ctx.bindValue(uiProp.observe(zip), BeanProperties.value("zip").observeDetail(master));
+		ctx.bindValue(uiProp.observe(city), BeanProperties.value("city").observeDetail(master));
+		
+		fadeOutTransition = new FadeTransition(Duration.millis(500), rootPane);
+        fadeOutTransition.setFromValue(1.0f);
+        fadeOutTransition.setToValue(0.0f);
+        fadeOutTransition.setAutoReverse(true);
+        
+        fadeInTransition = new FadeTransition(Duration.millis(500), rootPane);
+        fadeInTransition.setFromValue(0.0f);
+        fadeInTransition.setToValue(1.0f);
+        fadeInTransition.setAutoReverse(true);
+	}
+	
+	@Inject
+	public void setPerson(@Optional final Person person) {
+		if( fadeOutTransition != null ) {
+			fadeOutTransition.setOnFinished(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent arg0) {
+					master.setValue(person);
+					fadeOutTransition.setOnFinished(null);
+					fadeInTransition.playFromStart();
+				}
+			});
+			
+			fadeOutTransition.playFromStart();	
+		}
+	}
+}
