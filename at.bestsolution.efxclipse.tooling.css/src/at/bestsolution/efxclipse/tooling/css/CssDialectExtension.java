5507b70b4b2c565826328f67b351de5aabf404f7
Tom Schindl <tom.schindl@bestsolution.at>
2011-10-31 16:13:42 +0100

    fixed validation for "+" operator on numbers

--- at.bestsolution.efxclipse.tooling.css/src/at/bestsolution/efxclipse/tooling/css/CssDialectExtension.java
+++ at.bestsolution.efxclipse.tooling.css/src/at/bestsolution/efxclipse/tooling/css/CssDialectExtension.java
@@ -610,7 +610,8 @@ public interface CssDialectExtension {
 		}
 		
 		public static ValidationResult checkPercentage(term term, String message, int min) {
-			if( term.getNumber().matches("^\\d+(\\.\\d+)?%$") || term.getNumber().matches("^-\\d+(\\.\\d+)?%$") ) {
+			System.err.println("check percentage: " + term.getNumber());
+			if( term.getNumber().matches("^\\d+(\\.\\d+)?%$") || term.getNumber().matches("^-\\d+(\\.\\d+)?%$") || term.getNumber().matches("^\\+\\d+(\\.\\d+)?%$") ) {
 				double v = Double.parseDouble(term.getNumber().substring(0, term.getNumber().length()-1));
 				if( v < min * 1.0 || v > 100.0 ) {
 					return new ValidationResult(ValidationStatus.ERROR, message, term, null, -1);
