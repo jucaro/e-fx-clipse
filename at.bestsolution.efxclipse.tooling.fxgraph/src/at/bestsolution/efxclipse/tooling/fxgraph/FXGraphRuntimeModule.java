587cd8e84c5ca3f1cd36787559f6d24bcea046f9
Tom Schindl <tom.schindl@bestsolution.at>
2011-11-13 15:58:28 +0100

    adding back fxml generation as part of the build

--- at.bestsolution.efxclipse.tooling.fxgraph/src/at/bestsolution/efxclipse/tooling/fxgraph/FXGraphRuntimeModule.java
+++ at.bestsolution.efxclipse.tooling.fxgraph/src/at/bestsolution/efxclipse/tooling/fxgraph/FXGraphRuntimeModule.java
@@ -7,5 +7,8 @@ package at.bestsolution.efxclipse.tooling.fxgraph;
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class FXGraphRuntimeModule extends at.bestsolution.efxclipse.tooling.fxgraph.AbstractFXGraphRuntimeModule {
-
+	// contributed by org.eclipse.xtext.generator.generator.GeneratorFragment
+	public Class<? extends org.eclipse.xtext.generator.IGenerator> bindIGenerator() {
+		return at.bestsolution.efxclipse.tooling.fxgraph.generator.FXGraphGenerator.class;
+	}
 }
