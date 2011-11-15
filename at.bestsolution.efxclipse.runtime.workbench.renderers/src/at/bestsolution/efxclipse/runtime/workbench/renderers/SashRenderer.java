51284145e1efcdac2160f605272e71b57a1312bb
Kai Toedter <kai@toedter.com>
2011-11-15 16:32:20 +0100

    added listeners for sash model changes: orientation and weight

--- at.bestsolution.efxclipse.runtime.workbench.renderers/src/at/bestsolution/efxclipse/runtime/workbench/renderers/SashRenderer.java
+++ at.bestsolution.efxclipse.runtime.workbench.renderers/src/at/bestsolution/efxclipse/runtime/workbench/renderers/SashRenderer.java
@@ -6,27 +6,37 @@ import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.control.SplitPane;
 
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
 
 @SuppressWarnings("restriction")
 public class SashRenderer extends JFXRenderer {
+
+	private EventHandler sashOrientationHandler;
+	private EventHandler sashWeightHandler;
 
 	@Override
 	public Object createWidget(MUIElement element, Object parent) {
 		if (!(element instanceof MPartSashContainer)) {
 			return null;
 		}
-
+		MPartSashContainer partSashContainer = (MPartSashContainer) element;
 		SplitPane splitPane = new SplitPane();
 		Orientation orientation;
-		if (((MPartSashContainer) (MUIElement) element).isHorizontal()) {
+		if (partSashContainer.isHorizontal()) {
 			orientation = Orientation.HORIZONTAL;
 		} else {
 			orientation = Orientation.VERTICAL;
 		}
 		splitPane.setOrientation(orientation);
+
 		return splitPane;
 	}
 
@@ -62,5 +72,50 @@ public class SashRenderer extends JFXRenderer {
 		if (me instanceof MPartSashContainer) {
 			// TODO hook more?
 		}
+	}
+
+	@Inject
+	void postConstruct(IEventBroker eventBroker) {
+		sashOrientationHandler = new EventHandler() {
+			@Override
+			public void handleEvent(Event event) {
+				// Ensure that this event is for a MPartSashContainer
+				MUIElement element = (MUIElement) event.getProperty(UIEvents.EventTags.ELEMENT);
+				if (element.getRenderer() != SashRenderer.this) {
+					return;
+				}
+				Orientation orientation;
+				if (((MPartSashContainer) (MUIElement) element).isHorizontal()) {
+					orientation = Orientation.HORIZONTAL;
+				} else {
+					orientation = Orientation.VERTICAL;
+				}
+				((SplitPane) element.getWidget()).setOrientation(orientation);
+			}
+		};
+
+		eventBroker.subscribe(UIEvents.buildTopic(UIEvents.GenericTile.TOPIC, UIEvents.GenericTile.HORIZONTAL),
+				sashOrientationHandler);
+
+		sashWeightHandler = new EventHandler() {
+			@Override
+			public void handleEvent(Event event) {
+				// Ensure that this event is for a MPartSashContainer
+				MUIElement element = (MUIElement) event.getProperty(UIEvents.EventTags.ELEMENT);
+				if (element.getRenderer() != SashRenderer.this) {
+					return;
+				}
+
+				String dividerPos = element.getContainerData();
+				SplitPane splitPane = ((SplitPane) element.getWidget());
+				if (dividerPos != null && dividerPos != ((Double) splitPane.getDividerPositions()[0]).toString()) {
+					System.out.println("setting new splitpane divider pos in JavaFX: " + dividerPos);
+					splitPane.setDividerPositions(Float.parseFloat(dividerPos));
+				}
+			}
+		};
+
+		eventBroker.subscribe(UIEvents.buildTopic(UIEvents.UIElement.TOPIC, UIEvents.UIElement.CONTAINERDATA),
+				sashWeightHandler);
 	}
 }
