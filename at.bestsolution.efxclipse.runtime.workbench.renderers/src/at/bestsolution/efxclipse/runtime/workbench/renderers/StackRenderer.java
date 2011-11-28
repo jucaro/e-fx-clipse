c0cd3fbe3ed6940be61318a9d585e84d82b5e9ba
Kai Toedter <kai@toedter.com>
2011-11-28 16:23:05 +0100

    icons in tabs

--- at.bestsolution.efxclipse.runtime.workbench.renderers/src/at/bestsolution/efxclipse/runtime/workbench/renderers/StackRenderer.java
+++ at.bestsolution.efxclipse.runtime.workbench.renderers/src/at/bestsolution/efxclipse/runtime/workbench/renderers/StackRenderer.java
@@ -1,16 +1,20 @@
 package at.bestsolution.efxclipse.runtime.workbench.renderers;
 
+import java.net.URL;
+
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.MUILabel;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.IPresentationEngine;
+import org.eclipse.emf.common.util.URI;
 
 @SuppressWarnings("restriction")
 public class StackRenderer extends JFXRenderer {
@@ -25,11 +29,17 @@ public class StackRenderer extends JFXRenderer {
 	public void processContents(MElementContainer<MUIElement> container) {
 		TabPane parentPane = (TabPane) container.getWidget();
 
-		for (MUIElement e : container.getChildren()) {
-			final MUIElement tmp = e;
-			MUILabel mLabel = (MUILabel) e;
+		for (MUIElement element : container.getChildren()) {
+			final MUIElement tmp = element;
+			MUILabel mLabel = (MUILabel) element;
 			final Tab tab = new Tab(mLabel.getLocalizedLabel());
 			tab.setClosable(false); // TODO We need to read this from tags
+			if (mLabel.getIconURI() != null) {
+				URL url = Util.convertToOSGiURL(URI.createURI(mLabel.getIconURI()));
+				Image img = new Image(url.toExternalForm());
+				tab.setGraphic(new ImageView(img));
+			}
+
 			tab.setOnSelectionChanged(new EventHandler<Event>() {
 
 				@Override
@@ -43,39 +53,4 @@ public class StackRenderer extends JFXRenderer {
 			parentPane.getTabs().add(tab);
 		}
 	}
-
-	@Override
-	protected Object getParentWidget(MUIElement element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void disposeWidget(MUIElement part) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void hookControllerLogic(MUIElement me) {
-	}
-
-	@Override
-	public void childRendered(MElementContainer<MUIElement> parentElement, MUIElement element) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected Object getImage(MUILabel element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected boolean requiresFocus(MPart element) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
