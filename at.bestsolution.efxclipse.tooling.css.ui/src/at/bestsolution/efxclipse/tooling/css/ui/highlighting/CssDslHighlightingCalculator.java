731b18b2f75a7a6220a445e3c09301752f4443a9
Tom Schindl <tom.schindl@bestsolution.at>
2011-12-01 22:55:26 +0100

    fixed syntax highlighting

--- at.bestsolution.efxclipse.tooling.css.ui/src/at/bestsolution/efxclipse/tooling/css/ui/highlighting/CssDslHighlightingCalculator.java
+++ at.bestsolution.efxclipse.tooling.css.ui/src/at/bestsolution/efxclipse/tooling/css/ui/highlighting/CssDslHighlightingCalculator.java
@@ -19,7 +19,6 @@ import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
 import at.bestsolution.efxclipse.tooling.css.cssDsl.css_generic_declaration;
-import at.bestsolution.efxclipse.tooling.css.cssDsl.selector;
 import at.bestsolution.efxclipse.tooling.css.cssDsl.simple_selector;
 
 public class CssDslHighlightingCalculator implements ISemanticHighlightingCalculator {
@@ -27,19 +26,10 @@ public class CssDslHighlightingCalculator implements ISemanticHighlightingCalcul
 	@Override
 	public void provideHighlightingFor(XtextResource resource,
 			IHighlightedPositionAcceptor acceptor) {
-//		System.err.println("Called hightlighting calc");
 		if( resource == null ) {
 			return;
 		}
 		
-//		Iterator<INode> allNodes = resource.getParseResult().getRootNode().getAsTreeIterable().iterator();
-//		while(allNodes.hasNext()) {
-//			INode node = allNodes.next();
-//			if (node.getGrammarElement() instanceof CrossReference) {
-//				highlightNode(node, CssDslHighlightingConfiguration.CROSS_REF, acceptor);
-//			}
-//		}
-
 		TreeIterator<Object> it = EcoreUtil.getAllContents(resource, true);
 		
 		while( it.hasNext() ) {
@@ -49,29 +39,18 @@ public class CssDslHighlightingCalculator implements ISemanticHighlightingCalcul
 				css_generic_declaration dec = (css_generic_declaration) o;
 				if( dec.getProperty() != null && dec.getProperty().trim().length() > 0 ) {
 					ICompositeNode n = NodeModelUtils.getNode(dec);
-					acceptor.addPosition(n.getOffset(), dec.getProperty().length(), CssDslHighlightingConfiguration.DECLARATIONNAME);	
+					if( n.hasChildren() ) {
+						acceptor.addPosition(n.getFirstChild().getOffset(), n.getFirstChild().getLength(), CssDslHighlightingConfiguration.DECLARATIONNAME);
+					}	
 				}
 			} else if( o instanceof simple_selector ) {
 				simple_selector sec = (simple_selector) o;
 				ICompositeNode n = NodeModelUtils.getNode(sec);
-				acceptor.addPosition(n.getOffset(), n.getLength(), CssDslHighlightingConfiguration.SELECTOR);
-			} else {
-//				System.err.println(o);
+				
+				System.err.println(n.getOffset() + " => " + n.getText().trim().length());
+				
+				acceptor.addPosition(n.getOffset(), n.getText().trim().length(), CssDslHighlightingConfiguration.SELECTOR);
 			}
 		}
 	}
-	
-//	private void highlightNode(INode node, String id, IHighlightedPositionAcceptor acceptor) {
-//		if (node == null)
-//			return;
-//		if (node instanceof ILeafNode) {
-//			acceptor.addPosition(node.getOffset(), node.getLength(), id);
-//		} else {
-//			for(ILeafNode leaf: node.getLeafNodes()) {
-//				if (!leaf.isHidden()) {
-//					acceptor.addPosition(leaf.getOffset(), leaf.getLength(), id);
-//				}
-//			}
-//		}
-//	}
 }
