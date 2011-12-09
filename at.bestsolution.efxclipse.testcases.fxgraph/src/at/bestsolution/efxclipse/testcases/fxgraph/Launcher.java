2d535a64c28a10fbf4abbdf53ad9258b0cdb7063
Tom Schindl <tom.schindl@bestsolution.at>
2011-12-09 16:46:54 +0100

    adding test samples

--- at.bestsolution.efxclipse.testcases.fxgraph/src/at/bestsolution/efxclipse/testcases/fxgraph/Launcher.java
+++ at.bestsolution.efxclipse.testcases.fxgraph/src/at/bestsolution/efxclipse/testcases/fxgraph/Launcher.java
@@ -1,7 +1,22 @@
 package at.bestsolution.efxclipse.testcases.fxgraph;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Launcher extends Application {
 	public static void main(String[] args) {
-		
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent p = FXMLLoader.load(Launcher.class.getResource("IncludeTest.fxml"));
+		Scene s = new Scene(p);
+		primaryStage.setScene(s);
+		primaryStage.show();
 	}
 }
