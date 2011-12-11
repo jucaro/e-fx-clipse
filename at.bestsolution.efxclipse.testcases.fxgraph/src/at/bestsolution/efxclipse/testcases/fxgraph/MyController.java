8892ee01b37dc0569626673ee7830d39ce65a2e7
Tom Schindl <tom.schindl@bestsolution.at>
2011-12-11 19:29:22 +0100

    new test code

--- at.bestsolution.efxclipse.testcases.fxgraph/src/at/bestsolution/efxclipse/testcases/fxgraph/MyController.java
+++ at.bestsolution.efxclipse.testcases.fxgraph/src/at/bestsolution/efxclipse/testcases/fxgraph/MyController.java
@@ -3,6 +3,8 @@ package at.bestsolution.efxclipse.testcases.fxgraph;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class MyController implements Initializable {
@@ -12,4 +14,23 @@ public class MyController implements Initializable {
 		System.err.println("The controller is created!!!");
 	}
 
+	@FXML
+	public void clicked(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	public void clicked2(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	public void clicked(String event) {
+		
+	}
+	
+	@FXML
+	public void clicked() {
+		
+	}
 }
