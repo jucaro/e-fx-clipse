9f7d19ffbe10f574c34180fcd48b42e695333bcb
Tom Schindl <tom.schindl@bestsolution.at>
2011-12-21 23:18:03 +0100

    added test case for static attributes

--- at.bestsolution.efxclipse.testcases.fxgraph/src/at/bestsolution/efxclipse/testcases/fxgraph/MyController.java
+++ at.bestsolution.efxclipse.testcases.fxgraph/src/at/bestsolution/efxclipse/testcases/fxgraph/MyController.java
@@ -14,23 +14,35 @@ public class MyController implements Initializable {
 		System.err.println("The controller is created!!!");
 	}
 
+	/**
+	 * This the click 1 method
+	 * 
+	 * @param event
+	 *            the event
+	 */
 	@FXML
 	public void clicked(ActionEvent event) {
-		
+
 	}
-	
+
+	/**
+	 * This is the click 2 method
+	 * 
+	 * @param event
+	 *            the event
+	 */
 	@FXML
 	public void clicked2(ActionEvent event) {
-		
+
 	}
-	
-	@FXML
-	public void clicked(String event) {
-		
-	}
-	
-	@FXML
-	public void clicked() {
-		
-	}
+
+//	@FXML
+//	public void clicked(String event) {
+//
+//	}
+//
+//	@FXML
+//	public void clicked() {
+//
+//	}
 }
