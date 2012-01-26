a10569cf37a30e799a8d62f0fad8cad8af6795f8
Tom Schindl <tom.schindl@bestsolution.at>
2012-01-26 21:54:33 +0100

    commit for #26: showing JavaDoc

--- at.bestsolution.efxclipse.tooling.fxgraph.ui/src/at/bestsolution/efxclipse/tooling/fxgraph/ui/hover/FXHoverProvider.java
+++ at.bestsolution.efxclipse.tooling.fxgraph.ui/src/at/bestsolution/efxclipse/tooling/fxgraph/ui/hover/FXHoverProvider.java
@@ -1,17 +1,21 @@
 package at.bestsolution.efxclipse.tooling.fxgraph.ui.hover;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.internal.ui.text.java.hover.JavadocHover;
 import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.xtext.common.types.JvmType;
 import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.common.types.access.jdt.IJavaProjectProvider;
 import org.eclipse.xtext.common.types.util.jdt.IJavaElementFinder;
 import org.eclipse.xtext.xbase.ui.hover.XbaseHoverProvider;
 
@@ -20,6 +24,7 @@ import at.bestsolution.efxclipse.tooling.fxgraph.fXGraph.ControllerHandledValueP
 import at.bestsolution.efxclipse.tooling.fxgraph.fXGraph.Element;
 import at.bestsolution.efxclipse.tooling.fxgraph.fXGraph.Model;
 import at.bestsolution.efxclipse.tooling.fxgraph.fXGraph.Property;
+import at.bestsolution.efxclipse.tooling.fxgraph.fXGraph.SimpleValueProperty;
 import at.bestsolution.efxclipse.tooling.fxgraph.fXGraph.StaticValueProperty;
 import at.bestsolution.efxclipse.tooling.fxgraph.ui.util.JDTHelper;
 
@@ -49,10 +54,26 @@ public class FXHoverProvider extends XbaseHoverProvider {
 	@Inject
 	private IJavaElementFinder javaElementFinder;
 	
+	@Inject
+	private IJavaProjectProvider javaProjectProvider;
+	
 	private JavadocHoverWrapper javadocHover = new JavadocHoverWrapper ();
 	
 	@Override
 	public IInformationControlCreatorProvider getHoverInfo(EObject object, ITextViewer viewer, IRegion region) {
+		if( object instanceof SimpleValueProperty ) {
+			SimpleValueProperty v = (SimpleValueProperty) object;
+			if( v.getStringValue() != null ) {
+				if( v.eContainer() instanceof StaticValueProperty ) {
+					StaticValueProperty sProp = (StaticValueProperty) v.eContainer();
+					IType type = getEnumType(sProp);
+					if( type != null ) {
+						return findFieldJavaDoc(type, v.getStringValue(), object, viewer, region);
+					}
+				}
+			}
+		}
+		
 		if( object instanceof Property ) {
 			if( object.eContainer() instanceof Element ) {
 				Property property = (Property) object;
@@ -86,7 +107,8 @@ public class FXHoverProvider extends XbaseHoverProvider {
 				if( m != null ) {
 					ComponentDefinition def = m.getComponentDef();
 					if( def.getController() != null ) {
-						IInformationControlCreatorProvider rv = findFieldJavaDoc(def.getController().getType(), element.getName(),object,viewer,region);
+						IType jdtType = (IType) javaElementFinder.findElementFor(def.getController().getType());
+						IInformationControlCreatorProvider rv = findFieldJavaDoc(jdtType, element.getName(),object,viewer,region);
 						if( rv != null ) {
 							return rv; 
 						}
@@ -106,9 +128,7 @@ public class FXHoverProvider extends XbaseHoverProvider {
 		return super.getHoverInfo(object, viewer, region);
 	}
 	
-	private IInformationControlCreatorProvider findFieldJavaDoc(JvmType t, String fieldname, EObject object, ITextViewer viewer, IRegion region) {
-		IType jdtType = (IType) javaElementFinder.findElementFor(t);
-		
+	private IInformationControlCreatorProvider findFieldJavaDoc(IType jdtType, String fieldname, EObject object, ITextViewer viewer, IRegion region) {
 		try {
 			for( IField f : jdtType.getFields() ) {
 				if( f.getElementName().equals(fieldname) ) {
@@ -242,8 +262,42 @@ public class FXHoverProvider extends XbaseHoverProvider {
 	
 	@Override
 	protected boolean hasHover(EObject o) {
-		System.err.println(o);
+		if( o instanceof SimpleValueProperty ) {
+			SimpleValueProperty v = (SimpleValueProperty) o;
+			if( v.getStringValue() != null ) {
+				if( v.eContainer() instanceof StaticValueProperty ) {
+					StaticValueProperty sProp = (StaticValueProperty) v.eContainer();
+					return getEnumType(sProp) != null;
+				}
+			}
+		}
 		return super.hasHover(o);
+	}
+	
+	private IType getEnumType(StaticValueProperty sProp) {
+		IType t = (IType) javaElementFinder.findElementFor(sProp.getType().getType());
+		String methodName = "set" + Character.toUpperCase(sProp.getName().charAt(0)) + sProp.getName().substring(1);
+		try {
+			for( IMethod m : t.getMethods() ) {
+				if( Flags.isPublic(m.getFlags()) && Flags.isStatic(m.getFlags()) && m.getElementName().endsWith(methodName) && m.getParameterTypes().length == 2 ) {
+					String valueType = m.getParameterTypes()[1];
+					String fqType = Signature.toString(valueType);
+					IJavaProject jp = javaProjectProvider.getJavaProject(sProp.eResource().getResourceSet());
+					
+					IType type = jp.findType(fqType);
+					
+					if( type != null ) {
+						if( type.isEnum() ) {
+							return type;
+						}
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	static class JavadocHoverWrapper extends JavadocHover {
