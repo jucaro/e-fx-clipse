0939674285b28dacc63c88fba3093195825b1c56
Tom Schindl <tom.schindl@bestsolution.at>
2012-02-12 15:59:37 +0100

    * exporting more internal API * add start for a behavior implementation for FlingPane

---
+++ at.bestsolution.efxclipse.runtime.panels/src/at/bestsolution/efxclipse/runtime/panels/internal/FlingPaneBehavior.java
@@ -0,0 +1,13 @@
+package at.bestsolution.efxclipse.runtime.panels.internal;
+
+import at.bestsolution.efxclipse.runtime.panels.FlingPane;
+
+import com.sun.javafx.scene.control.behavior.BehaviorBase;
+
+@SuppressWarnings("restriction")
+public class FlingPaneBehavior /* extends BehaviorBase<FlingPane>*/ {
+
+//	public FlingPaneBehavior(FlingPane pane) {
+//		super(pane);
+//	}
+}
