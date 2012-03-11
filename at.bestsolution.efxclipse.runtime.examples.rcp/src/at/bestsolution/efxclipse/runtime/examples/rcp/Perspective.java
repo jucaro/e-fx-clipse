74168874c2c811c40ef2607f095491aa5b7044e4
Tom Schindl <tom.schindl@bestsolution.at>
2012-03-11 18:51:46 +0100

    adding example for new deployment model

---
+++ at.bestsolution.efxclipse.runtime.examples.rcp/src/at/bestsolution/efxclipse/runtime/examples/rcp/Perspective.java
@@ -0,0 +1,14 @@
+package at.bestsolution.efxclipse.runtime.examples.rcp;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+		
+	}
+
+}
