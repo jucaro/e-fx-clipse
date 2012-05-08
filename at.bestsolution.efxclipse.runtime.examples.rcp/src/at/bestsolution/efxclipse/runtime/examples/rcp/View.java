84b21477b9819f29dfdd30d80a699398b39c7e12
Tom Schindl <tom.schindl@bestsolution.at>
2012-05-08 13:42:15 +0200

    use generic base class

--- at.bestsolution.efxclipse.runtime.examples.rcp/src/at/bestsolution/efxclipse/runtime/examples/rcp/View.java
+++ at.bestsolution.efxclipse.runtime.examples.rcp/src/at/bestsolution/efxclipse/runtime/examples/rcp/View.java
@@ -4,7 +4,6 @@ import static java.lang.Math.random;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.embed.swt.FXCanvas;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -19,36 +18,17 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.util.Duration;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.ViewPart;
+import at.bestsolution.efxclipse.runtime.workbench3.FXViewPart;
 
-public class View extends ViewPart {
+public class View extends FXViewPart {
 	public static final String ID = "at.bestsolution.efxclipse.runtime.examples.rcp.view";
 
-	private FXCanvas canvas;
-
-	/**
-	 * This is a callback that will allow us to create the viewer and initialize
-	 * it.
-	 */
-	public void createPartControl(Composite parent) {
-		canvas = new FXCanvas(parent, SWT.NONE);
-		
+	@Override
+	protected Scene createFxScene() {
 		Group root = new Group();
-		Scene scene = new Scene(root,800,600,Color.BLACK);
-		canvas.setScene(scene);
-		
-		Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(),
-				new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE,
-						new Stop[] { new Stop(0, Color.web("#f8bd55")),
-								new Stop(0.14, Color.web("#c0fe56")),
-								new Stop(0.28, Color.web("#5dfbc1")),
-								new Stop(0.43, Color.web("#64c2f8")),
-								new Stop(0.57, Color.web("#be4af7")),
-								new Stop(0.71, Color.web("#ed5fc2")),
-								new Stop(0.85, Color.web("#ef504c")),
-								new Stop(1, Color.web("#f2660f")), }));
+		Scene scene = new Scene(root, 800, 600, Color.BLACK);
+		Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(), new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new Stop[] { new Stop(0, Color.web("#f8bd55")), new Stop(0.14, Color.web("#c0fe56")), new Stop(0.28, Color.web("#5dfbc1")),
+				new Stop(0.43, Color.web("#64c2f8")), new Stop(0.57, Color.web("#be4af7")), new Stop(0.71, Color.web("#ed5fc2")), new Stop(0.85, Color.web("#ef504c")), new Stop(1, Color.web("#f2660f")), }));
 
 		Group circles = new Group();
 		for (int i = 0; i < 30; i++) {
@@ -60,34 +40,31 @@ public class View extends ViewPart {
 		}
 		circles.setEffect(new BoxBlur(10, 10, 3));
 
-		Group blendModeGroup = new Group(new Group(new Rectangle(
-				scene.getWidth(), scene.getHeight(), Color.BLACK), circles),
-				colors);
+		Group blendModeGroup = new Group(new Group(new Rectangle(scene.getWidth(), scene.getHeight(), Color.BLACK), circles), colors);
 		colors.setBlendMode(BlendMode.OVERLAY);
 		root.getChildren().add(blendModeGroup);
 
 		Timeline timeline = new Timeline();
 		for (Node circle : circles.getChildren()) {
-			timeline.getKeyFrames().addAll(
-					new KeyFrame(Duration.ZERO, // set start position at 0
-							new KeyValue(circle.translateXProperty(),
-									random() * 800),
-							new KeyValue(circle.translateYProperty(),
-									random() * 600)),
-					new KeyFrame(new Duration(40000), // set end position at 40s
-							new KeyValue(circle.translateXProperty(),
-									random() * 800), new KeyValue(circle
-									.translateYProperty(), random() * 600)));
+			timeline.getKeyFrames().addAll(new KeyFrame(Duration.ZERO, // set
+																		// start
+																		// position
+																		// at 0
+					new KeyValue(circle.translateXProperty(), random() * 800), new KeyValue(circle.translateYProperty(), random() * 600)), new KeyFrame(new Duration(40000), // set
+																																												// end
+																																												// position
+																																												// at
+																																												// 40s
+					new KeyValue(circle.translateXProperty(), random() * 800), new KeyValue(circle.translateYProperty(), random() * 600)));
 		}
 		// play 40s of animation
-		timeline.play(); 
-
+		timeline.play();
+		return scene;
 	}
 
-	/**
-	 * Passing the focus request to the viewer's control.
-	 */
-	public void setFocus() {
-		canvas.setFocus();
+	@Override
+	protected void setFxFocus() {
+		// TODO Auto-generated method stub
+
 	}
 }
