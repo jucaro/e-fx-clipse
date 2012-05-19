bd05e758a946cbe259c8ca17f061923f17d855a6
Tom Schindl <tom.schindl@bestsolution.at>
2012-05-19 11:21:59 +0200

    implemented autocompletetion and auto imports for types

--- at.bestsolution.efxclipse.tooling.fxml/src/at/bestsolution/efxclipse/tooling/fxml/editors/FXMLCompletionProposalComputer.java
+++ at.bestsolution.efxclipse.tooling.fxml/src/at/bestsolution/efxclipse/tooling/fxml/editors/FXMLCompletionProposalComputer.java
@@ -8,6 +8,7 @@ import java.util.Vector;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EcoreFactory;
@@ -16,12 +17,23 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.TypeNameRequestor;
 import org.eclipse.jdt.internal.ui.text.java.hover.JavadocHover;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.DocumentRewriteSession;
+import org.eclipse.jface.text.DocumentRewriteSessionType;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentExtension4;
 import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextHoverExtension;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocumentRegion;
 import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
 import org.eclipse.wst.sse.ui.contentassist.CompletionProposalInvocationContext;
@@ -29,6 +41,10 @@ import org.eclipse.wst.sse.ui.contentassist.ICompletionProposalComputer;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
 import org.eclipse.wst.xml.ui.internal.contentassist.AbstractXMLCompletionProposalComputer;
 import org.eclipse.wst.xml.ui.internal.contentassist.ContentAssistRequest;
+import org.eclipse.xtext.common.types.xtext.ui.IntersectingJavaSearchScope;
+import org.eclipse.xtext.ui.editor.contentassist.ConfigurableCompletionProposal;
+import org.eclipse.xtext.ui.editor.contentassist.ConfigurableCompletionProposal.IReplacementTextApplier;
+import org.eclipse.xtext.ui.editor.contentassist.FQNPrefixMatcher;
 import org.eclipse.xtext.ui.editor.contentassist.PrefixMatcher;
 import org.eclipse.xtext.ui.editor.contentassist.PrefixMatcher.CamelCase;
 import org.eclipse.xtext.ui.editor.hover.IEObjectHover;
@@ -49,59 +65,29 @@ import at.bestsolution.efxclipse.tooling.ui.util.IconKeys;
 
 @SuppressWarnings("restriction")
 public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposalComputer implements ICompletionProposalComputer {
-	private static final PrefixMatcher MATCHER = new CamelCase();
-	public FXMLCompletionProposalComputer() {
-		System.err.println("Created proposal provider");
-		// TODO Auto-generated constructor stub
+	private static final CamelCase MATCHER = new CamelCase();
+	private static final FQNPrefixMatcher FQN_MATCHER = new FQNPrefixMatcher();
+	
+	{
+		FQN_MATCHER.setLastSegmentFinder(new FQNPrefixMatcher.DefaultLastSegmentFinder());
+		FQN_MATCHER.setDelegate(MATCHER);
 	}
-
+	
 	@Override
 	public void sessionStarted() {
-		// TODO Auto-generated method stub
-
 	}
-
-	// @SuppressWarnings("rawtypes")
-	// @Override
-	// public List
-	// computeCompletionProposals(CompletionProposalInvocationContext context,
-	// IProgressMonitor monitor) {
-	// System.err.println(context);
-	// ConfigurableCompletionProposal prop = new
-	// ConfigurableCompletionProposal("bla", 0, "bla".length(),
-	// context.getInvocationOffset());
-	// // CompletionProposal prop = new CompletionProposal("bla", 0,
-	// "bla".length(), context.getInvocationOffset());
-	//
-	// // TODO Auto-generated method stub
-	// return Collections.singletonList(prop);
-	// }
-	//
-	// @Override
-	// public List computeContextInformation(CompletionProposalInvocationContext
-	// context, IProgressMonitor monitor) {
-	// System.err.println("Get context infos");
-	// // TODO Auto-generated method stub
-	// return Collections.emptyList();
-	// }
 
 	@Override
 	public String getErrorMessage() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void sessionEnded() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	protected void addAttributeNameProposals(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
-		// TODO Auto-generated method stub
-		System.err.println("Adding attribiute proposal");
-
 		String typeName = null;
 		Node parent = contentAssistRequest.getParent();
 		if (parent.getNodeType() == Node.ELEMENT_NODE) {
@@ -242,10 +228,11 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 		return null;
 	}
 	
-	private FXMLCompletionProposal createElementProposal(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context, String proposalValue, StyledString s, boolean withEnd) {
-		if( MATCHER.isCandidateMatchingPrefix(proposalValue, contentAssistRequest.getMatchString()) ) {
-			FXMLCompletionProposal cp = new FXMLCompletionProposal(proposalValue, context.getInvocationOffset()-contentAssistRequest.getMatchString().length(), proposalValue.length(), withEnd ? proposalValue.length()/2-1 : proposalValue.length(), IconKeys.getIcon(IconKeys.FIELD_KEY), s, null);
-			cp.setMatcher(new CamelCase());
+	private FXMLCompletionProposal createElementProposal(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context, String proposalValue, StyledString s, boolean withEnd, int priority, Image img, PrefixMatcher m) {
+		if( m.isCandidateMatchingPrefix(proposalValue, contentAssistRequest.getMatchString()) ) {
+			FXMLCompletionProposal cp = new FXMLCompletionProposal(proposalValue, context.getInvocationOffset()-contentAssistRequest.getMatchString().length(), proposalValue.length(), withEnd ? proposalValue.length()/2-1 : proposalValue.length(), img, s, null);
+			cp.setPriority(priority);
+			cp.setMatcher(m);
 			return cp;	
 		}
 		return null;
@@ -257,17 +244,61 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 		
 		if( parent.getNodeType() == Node.ELEMENT_NODE ) {
 			if( Character.isUpperCase(parent.getNodeName().charAt(0)) ) {
-				IType type = findType(parent.getNodeName(), contentAssistRequest, context);
-				if( type != null ) {
-					IFXClass fxClass = FXPlugin.getClassmodel().findClass(type.getJavaProject(), type);
-					if( fxClass != null ) {
-						for( IFXProperty property : fxClass.getAllProperties().values() ) {
-							createPropertyElementNameProposal(contentAssistRequest, context, property);
+				if( !contentAssistRequest.getMatchString().isEmpty() && Character.isUpperCase(contentAssistRequest.getMatchString().charAt(0)) ) {
+					IJavaProject jproject = findProject(contentAssistRequest);
+					try {
+						IType superType = jproject.findType("javafx.scene.Parent");
+						if( superType != null ) {
+							createSubtypeProposals(contentAssistRequest, context, superType);	
 						}
+					} catch (JavaModelException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
+				} else {
+					IJavaProject jproject = findProject(contentAssistRequest);
+					try {
+						IType superType = jproject.findType("javafx.scene.Parent");
+						if( superType != null ) {
+							createSubtypeProposals(contentAssistRequest, context, superType);	
+						}
+					} catch (JavaModelException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					IType type = findType(parent.getNodeName(), contentAssistRequest, context);
+					if( type != null ) {
+						IFXClass fxClass = FXPlugin.getClassmodel().findClass(type.getJavaProject(), type);
+						if( fxClass != null ) {
+							for( IFXProperty property : fxClass.getAllProperties().values() ) {
+								createPropertyElementNameProposal(contentAssistRequest, context, property);
+							}
+						}
+					}					
 				}
 			} else {
-				System.err.println("Find element");
+				createClassElementNameProposal(contentAssistRequest, context);
+			}
+		}
+	}
+	
+	private void createClassElementNameProposal(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
+		Node propertyType = contentAssistRequest.getParent();
+		Node elementType = propertyType.getParentNode();
+		
+		IType type = findType(elementType.getNodeName(), contentAssistRequest, context);
+		if( type != null ) {
+			IFXClass fxClass = FXPlugin.getClassmodel().findClass(type.getJavaProject(), type);
+			if( fxClass != null ) {
+				IFXProperty p = fxClass.getProperty(propertyType.getNodeName());
+				if( p instanceof IFXObjectProperty ) {
+					IFXObjectProperty op = (IFXObjectProperty) p;
+					createSubtypeProposals(contentAssistRequest, context, op.getElementType());
+				} else if( p instanceof IFXCollectionProperty ) {
+					IFXCollectionProperty cp = (IFXCollectionProperty) p;
+					createSubtypeProposals(contentAssistRequest, context, cp.getElementType());
+				}
 			}
 		}
 	}
@@ -287,7 +318,7 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 		
 		String propValue = prop.getName() + "></"+prop.getName()+">";
 		
-		FXMLCompletionProposal cp = createElementProposal(contentAssistRequest, context, propValue, s, true); 
+		FXMLCompletionProposal cp = createElementProposal(contentAssistRequest, context, propValue, s, true, 200, IconKeys.getIcon(IconKeys.FIELD_KEY),MATCHER); 
 		if( cp != null ) {
 			cp.setAdditionalProposalInfo(EcoreFactory.eINSTANCE.createEClass());
 			cp.setHover(new HoverImpl(prop.getJavaElement()));
@@ -302,7 +333,7 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 		
 		String propValue = prop.getName() + "></"+prop.getName()+">";
 		
-		FXMLCompletionProposal cp = createElementProposal(contentAssistRequest, context, propValue, s, true); 
+		FXMLCompletionProposal cp = createElementProposal(contentAssistRequest, context, propValue, s, true, 200, IconKeys.getIcon(IconKeys.FIELD_KEY),MATCHER); 
 		if( cp != null ) {
 			cp.setAdditionalProposalInfo(EcoreFactory.eINSTANCE.createEClass());
 			cp.setHover(new HoverImpl(prop.getJavaElement()));
@@ -311,9 +342,123 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 		}
 	}
 	
+	private void createSubtypeProposals(final ContentAssistRequest contentAssistRequest, final CompletionProposalInvocationContext context, IType superType) {
+		IJavaProject jproject = findProject(contentAssistRequest);
+		
+		try {
+			IJavaSearchScope hierarchyScope = SearchEngine.createHierarchyScope(superType);
+			IJavaSearchScope projectScope = SearchEngine.createJavaSearchScope(new IJavaElement[] { jproject });
+			IJavaSearchScope scope = new IntersectingJavaSearchScope(projectScope, hierarchyScope);
+			SearchEngine searchEngine = new SearchEngine();
+			
+			char[] typeName = null;
+			char[] packageName = null;
+			if( ! contentAssistRequest.getMatchString().isEmpty()) {
+				typeName = contentAssistRequest.getMatchString().toCharArray();
+			}
+			
+			searchEngine.searchAllTypeNames(packageName, SearchPattern.R_PATTERN_MATCH, 
+					typeName, SearchPattern.R_PREFIX_MATCH | SearchPattern.R_CAMELCASE_MATCH, 
+					IJavaSearchConstants.CLASS, scope, 
+					new TypeNameRequestor() {
+						@Override
+						public void acceptType(int modifiers, char[] packageName, char[] simpleTypeName, char[][] enclosingTypeNames, String path) {
+							String sPackageName = new String(packageName);
+							int priority = 100;
+							if( sPackageName.startsWith("com.sun.javafx") ) {
+								priority -= 10; 
+							}
+							StyledString s = new StyledString(new String(simpleTypeName));
+							s.append(" - " + sPackageName, StyledString.QUALIFIER_STYLER);
+							FXMLCompletionProposal prop = createElementProposal(contentAssistRequest, context, sPackageName + "." + new String(simpleTypeName) + " ", s, false, priority, IconKeys.getIcon(IconKeys.CLASS_KEY),FQN_MATCHER);
+							
+							if( prop != null ) {
+								prop.setMatcher(FQN_MATCHER);
+								prop.setTextApplier(new IReplacementTextApplier() {
+									
+									@Override
+									public void apply(IDocument document, ConfigurableCompletionProposal proposal) throws BadLocationException {
+										String proposalReplacementString = proposal.getReplacementString();
+										List<String> s = getImportedTypes(contentAssistRequest);
+										String shortened = proposalReplacementString.substring(proposalReplacementString.lastIndexOf('.')+1);
+										
+										if( s.contains(proposalReplacementString.trim()) ) {
+											proposal.setCursorPosition(shortened.length());
+											document.replace(proposal.getReplacementOffset(), proposal.getReplacementLength(), shortened);
+										} else {
+											DocumentRewriteSession rewriteSession = null;
+											try {
+												if (document instanceof IDocumentExtension4) {
+													rewriteSession = ((IDocumentExtension4) document).startRewriteSession(DocumentRewriteSessionType.UNRESTRICTED_SMALL);
+												}
+												
+											} finally {
+												if (rewriteSession != null) {
+													((IDocumentExtension4) document).stopRewriteSession(rewriteSession);
+												}
+//												if (viewerExtension != null)
+//													viewerExtension.setRedraw(true);
+											}
+											
+											boolean startWithLineBreak = true;
+											boolean endWithLineBreak = false;
+											Document xmlDoc = contentAssistRequest.getNode().getOwnerDocument();
+											NodeList list = xmlDoc.getChildNodes();
+											int offset = 0;
+											List<Node> prs = new ArrayList<Node>();
+											for (int i = 0; i < list.getLength(); i++) {
+												Node n = list.item(i);
+												if (n.getNodeType() == Node.PROCESSING_INSTRUCTION_NODE) {
+													prs.add(n);
+												}
+											}
+											
+											if( ! prs.isEmpty() ) {
+												offset = ((IDOMNode)prs.get(prs.size()-1)).getEndOffset();	
+											}
+											
+											
+											offset = Math.min(proposal.getReplacementOffset(), offset);
+											proposal.setCursorPosition(shortened.length());
+											document.replace(proposal.getReplacementOffset(), proposal.getReplacementLength(), shortened);
+											
+											String importStatement = (startWithLineBreak ? "\n<?import " : "import ") + proposalReplacementString.trim();
+											importStatement += "?>";
+											
+											if (endWithLineBreak)
+												importStatement += "\n\n";
+											document.replace(offset, 0, importStatement.toString());
+											proposal.setCursorPosition(proposal.getCursorPosition() + importStatement.length());	
+											
+//											document.replace(proposal.getReplacementOffset(), proposal.getReplacementLength(), proposal.getReplacementString());	
+										}
+									}
+								});
+								contentAssistRequest.addProposal(prop);
+							}
+						}
+					}, IJavaSearchConstants.WAIT_UNTIL_READY_TO_SEARCH, new NullProgressMonitor());
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	
-	
+	@Override
+	protected void addStartDocumentProposals(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
+		IJavaProject jproject = findProject(contentAssistRequest);
+		try {
+			IType superType = jproject.findType("javafx.scene.Parent");
+			if( superType != null ) {
+				createSubtypeProposals(contentAssistRequest, context, superType);
+			}
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	private static boolean isIntegerType(String fqnType) {
 		if( "java.lang.Integer".equals(fqnType) ) {
@@ -337,6 +482,14 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 		return false;
 	}
 
+	private IJavaProject findProject(ContentAssistRequest contentAssistRequest) {
+		Document xmlDoc = contentAssistRequest.getNode().getOwnerDocument();
+
+		String baseLocation = ((IDOMNode) xmlDoc).getModel().getBaseLocation();
+		IFile f = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(baseLocation));
+		return JavaCore.create(f.getProject());
+	}
+	
 	private IType findType(String name, ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
 		Document xmlDoc = contentAssistRequest.getNode().getOwnerDocument();
 		NodeList list = xmlDoc.getChildNodes();
@@ -389,6 +542,24 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 
 		return null;
 	}
+	
+	private List<String> getImportedTypes(ContentAssistRequest contentAssistRequest) {
+		Document xmlDoc = contentAssistRequest.getNode().getOwnerDocument();
+		NodeList list = xmlDoc.getChildNodes();
+
+		List<String> imports = new ArrayList<String>();
+		for (int i = 0; i < list.getLength(); i++) {
+			Node n = list.item(i);
+			if (n.getNodeType() == Node.PROCESSING_INSTRUCTION_NODE) {
+				String data = ((ProcessingInstruction) n).getData();
+				if (data.endsWith("?")) {
+					data = data.substring(0, data.length() - 1);
+				}
+				imports.add(data);
+			}
+		}
+		return imports;
+	}
 
 	@Override
 	protected void addAttributeValueProposals(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
@@ -413,10 +584,6 @@ public class FXMLCompletionProposalComputer extends AbstractXMLCompletionProposa
 	@Override
 	protected void addPCDATAProposal(String nodeName, ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
 
-	}
-
-	@Override
-	protected void addStartDocumentProposals(ContentAssistRequest contentAssistRequest, CompletionProposalInvocationContext context) {
 	}
 
 	@Override
