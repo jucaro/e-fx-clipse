1a6b2dd686365d816dc531053013c748bd320092
Tom Schindl <tom.schindl@bestsolution.at>
2012-06-14 15:07:19 +0200

    adding wizard to bootstrap a 3.x RCP application

--- at.bestsolution.efxclipse.tooling.pde.ui/src/at/bestsolution/efxclipse/tooling/pde/ui/E3PluginTemplate.java
+++ at.bestsolution.efxclipse.tooling.pde.ui/src/at/bestsolution/efxclipse/tooling/pde/ui/E3PluginTemplate.java
@@ -147,60 +147,11 @@ public class E3PluginTemplate extends FXPDETemplateSection {
 				imports = "";
 			}
 			
-			imports += getCommaValuesFromPackagesArray(getImports(), null);
+			imports += Activator.getCommaValuesFromPackagesArray(Activator.getJavaFXImports(), null);
 			
 			bundle.setHeader(Constants.IMPORT_PACKAGE, imports);
 		}
 		
 		super.execute(project, model, monitor);
-	}
-	
-	protected String getCommaValuesFromPackagesArray(String[] values, String version) {
-		StringBuffer buffer = new StringBuffer();
-		for (String value : values) {
-			if (buffer.length() > 0) {
-				buffer.append(",\n "); //$NON-NLS-1$ // space required for multiline headers
-			}
-			buffer.append(value);
-
-			if (value.indexOf(";version=") == -1 && (version != null) && (values.length == 1)) { //$NON-NLS-1$
-				buffer.append(";version=\"").append(version).append("\""); //$NON-NLS-1$ //$NON-NLS-2$
-			}
-		}
-		return buffer.toString();
-	}
-	
-	
-	private static String[] getImports() {
-		return new String[] {
-				"javafx.animation;version=\"2.0.0\"",
-				"javafx.application;version=\"2.0.0\"",
-				"javafx.beans;version=\"2.0.0\"",
-				"javafx.beans.binding;version=\"2.0.0\"",
-				"javafx.beans.property;version=\"2.0.0\"",
-				"javafx.beans.value;version=\"2.0.0\"",
-				"javafx.collections;version=\"2.0.0\"",
-				"javafx.concurrent;version=\"2.0.0\"",
-				"javafx.embed.swing;version=\"2.0.0\"",
-				"javafx.event;version=\"2.0.0\"",
-				"javafx.fxml;version=\"2.0.0\"",
-				"javafx.geometry;version=\"2.0.0\"",
-				"javafx.scene;version=\"2.0.0\"",
-				"javafx.scene.chart;version=\"2.0.0\"",
-				"javafx.scene.control;version=\"2.0.0\"",
-				"javafx.scene.control.cell;version=\"2.0.0\"",
-				"javafx.scene.effect;version=\"2.0.0\"",
-				"javafx.scene.image;version=\"2.0.0\"",
-				"javafx.scene.input;version=\"2.0.0\"",
-				"javafx.scene.layout;version=\"2.0.0\"",
-				"javafx.scene.media;version=\"2.0.0\"",
-				"javafx.scene.paint;version=\"2.0.0\"",
-				"javafx.scene.shape;version=\"2.0.0\"",
-				"javafx.scene.text;version=\"2.0.0\"",
-				"javafx.scene.transform;version=\"2.0.0\"",
-				"javafx.scene.web;version=\"2.0.0\"",
-				"javafx.stage;version=\"2.0.0\"",
-				"javafx.util;version=\"2.0.0\""
-			};
 	}
 }
